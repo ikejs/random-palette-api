17f73fda719c69af64984c7b6cc2ffa5895618f9
Ike <ikescode@gmail.com>
2020-05-07 20:01:56 -0500

    rearrange hues

--- controllers/random.js
+++ controllers/random.js
@@ -1,11 +1,22 @@
 exports.getRandomHSL = ((req, res) => {
-  res.send({
-      1: "#A9AA2F",
-      2: "#C77D15",
-      3: "#7E2F1D",
-      4: "#8A2918",
-      5: "#36181B"
-    });
+  // base color
+  const h = Math.round(Math.random() * 360)
+  const s = Math.round(Math.random() * 100)
+  const l = Math.round(Math.random() * 50)
+
+  // hue varieties
+  const h2 = (h + 20) % 360
+  const h1 = (h + 90) % 360
+  const h4 = (h + 180) % 360
+  const h3 = (h + 340) % 360
+
+  res.json({
+    first: `hsl(${h1}, ${s}%, ${l}%)`,
+    second: `hsl(${h2}, ${s}%, ${l}%)`,
+    third: `hsl(${h}, ${s}%, ${l}%)`,
+    fourth: `hsl(${h3}, ${s}%, ${l}%)`,
+    fifth: `hsl(${h4}, ${s}%, ${l}%)`
+  });
 });
 
 exports.getRandomHSLfromHEX = ((req, res) => {
